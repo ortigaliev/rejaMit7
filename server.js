e8211b6446f16772f0f40beb77df45836fe4f8f7
ortigaliev <ortigaliyevm@gmail.com>
2023-09-23 01:57:21 +0900

    EJS framework orqali ananaviy frontend qurami

--- server.js
+++ server.js
@@ -4,21 +4,36 @@ const express = require("express");
 const app = express();//expressning app objectini yuboradi
 const http = require("http");
 
+// 1: Kirish code
 
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// 2: Session Code
 
+
+// 3: View Code
 
 app.set("views","views");
 app.set("view engine","ejs");
 
+// 4: Routing code
 
-
-app.get("/", function(req,res) {
-res.end("Hello world");
+app.post("/create-item", (req, res) => {
+  console.log(req)
+  res.json({test: "success"});
 });
+
+app.get('/', function (req, res) {
+    res.render('harid');
+});
+
+/* //Portimizda router yasaymiz
+app.get("/", function(req,res) {
+  res.render("project");
+  });
+ */
 
 const server = http.createServer(app);
 let PORT = 3000;
