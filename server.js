2879276e8b8a1d1a4a6f049bc0a15e981c1c8c38
ortigaliev <ortigaliyevm@gmail.com>
2023-10-03 12:35:11 +0900

    Fix reja and app files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const mongodb = require('mongodb');
 
 let db;
-const connectionString = "mongodb+srv://ortigaliyevm:NqRWKCEJPpsAjhb@ortigaliyev.yri8ho8.mongodb.net/?retryWrites=true&w=majority";
+const connectionString = "mongodb+srv://Max_Mit7:ONHKVnH0aCQu9PQ0@mit-7.fpdtxon.mongodb.net/?retryWrites=true&w=majority";
 
 mongodb.connect(
   connectionString,
