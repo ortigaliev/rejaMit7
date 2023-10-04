889177ae8a8f9320b25dffdd9171d8a52ebecf79
ortigaliev <ortigaliyevm@gmail.com>
2023-10-05 05:52:17 +0900

    26. Rejalarni o'zgartirish (UPDATE)

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,5 @@
+
+
 console.log("FrontEnd ishga tushdi");
 
 function itemTemplate(item) {
@@ -52,7 +54,29 @@ document.addEventListener("click", function (e) {
   //edit oper
 
   if(e.target.classList.contains("edit-me")){
-    alert("Siz edit tugmasini bosdingiz!");
-  }
+    let userInput = prompt("O'zgarish kiriting",
+    e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+    if(userInput) {
+      axios.post("/edit-item", {
+        id: e.target.getAttribute("data-id"),
+        new_input: userInput,
+      }).then(response => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text"
+          ).innerHTML = userInput;
 
+      }).catch(err => {
+        console.log("Iltimos qaytadan xarakat qiling!");
+      });
+    }
+  }
+});
+
+/* DELETE ALL BUTTON */
+document.getElementById("clean-all").addEventListener("click", function() {
+  axios.post("/deleteall", {delete_all: true}).then(response => {
+    alert(response.data.state);
+    document.location.reload();
+  });
 });
