c8e8369e545adee440b97f541ca805afe5e718b3
ortigaliev <ortigaliyevm@gmail.com>
2023-10-05 02:37:56 +0900

    25. Rejani o'shirish (DELETE)

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();//expressning app objectini yuboradi
 
 //MongoDB connect chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1: Kirish code
 
@@ -29,6 +30,14 @@ app.post("/create-item", (req, res) => {
     console.log(data.ops);
     res.json(data.ops[0]);
   });
+});
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+  db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data){
+    res.json({state: "success"});
+  }
+  );
 });
 
 app.get('/', function (req, res) {
--- public/browser.js
+++ public/browser.js
@@ -30,3 +30,29 @@ document.getElementById("create-form").addEventListener("submit", function (e){
     console.log("Iltimos qaytadan harakat qiling");
   });
 });
+
+document.addEventListener("click", function (e) {
+
+  //delete oper
+  console.log(e.target);
+  if(e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq o'chirmoqchimisiz?")) {
+      axios
+      .post("/delete-item", {id: e.target.getAttribute("data-id") })
+      .then((response) => {
+        console.log(response.data);
+        e.target.parentElement.parentElement.remove();
+      })
+      .catch((err) => {
+        console.log("Iltimos qaytadan harakat qiling!");
+      });
+    }
+  }
+
+  //edit oper
+
+  if(e.target.classList.contains("edit-me")){
+    alert("Siz edit tugmasini bosdingiz!");
+  }
+
+});
